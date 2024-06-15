0482c7bbecb9676f845202b3479b1d00ceb12f2a
AbhinavNaman <abhinavnaman3@gmail.com>
2024-06-16 01:58:07 +0530

    _

--- api/generate-mcqs.js
+++
@@ -1,7 +0,0 @@
-import { createProxyMiddleware } from 'http-proxy-middleware';
-
-export default createProxyMiddleware({
-  target: 'http://4.227.155.222:8090',
-  changeOrigin: true,
-  pathRewrite: { '^/api/generate-mcqs': '/generate-mcqs' },
-});
